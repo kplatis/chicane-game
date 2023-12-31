86f141060717a7fbb8fdc727da1897ec1c1b5c96
kplatis <kplatis@protonmail.com>
2023-12-31 15:00:54 +0200

    Initialize api

---
+++ api/main.py
@@ -0,0 +1,30 @@
+from datetime import date
+from fastapi import FastAPI, HTTPException
+from tortoise import Tortoise
+from tortoise.exceptions import DoesNotExist
+from api.models import Game
+
+
+app = FastAPI()
+
+
+async def init():
+    await Tortoise.init(
+        db_url='sqlite://db.sqlite3',
+        modules={'models': ['api.models']},
+    )
+    await Tortoise.generate_schemas()
+
+
+@app.on_event("startup")
+async def startup_event():
+    await init()
+
+
+@app.get("/games/daily")
+async def root():
+    try:
+        db_item = await Game.get(date=date.today())
+        return db_item.__dict__
+    except DoesNotExist:
+        raise HTTPException(status_code=404, detail="Game not found")
---
+++ api/models.py
@@ -0,0 +1,22 @@
+from tortoise import fields, Model
+
+
+class Game(Model):
+    id = fields.IntField(pk=True)
+    question = fields.CharField(max_length=300)
+    date = fields.DateField()
+    category = fields.ForeignKeyField("models.GameCategory", related_name="games")
+
+
+class GameCategory(Model):
+    id = fields.IntField(pk=True)
+    title = fields.CharField(max_length=100)
+    games = fields.ReverseRelation["Game"]
+
+
+class Response(Model):
+    id = fields.IntField(pk=True)
+    category = fields.ForeignKeyField("models.GameCategory", related_name="responses")
+    title = fields.CharField(max_length=300)
+
+
