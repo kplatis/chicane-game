b8336eb03011e3c6612c4ef97b4aae9197da6bac
kplatis <kplatis@protonmail.com>
2024-02-04 20:28:21 +0100

    Change files to app from pages

--- frontend/src/components/Welcome/Welcome.tsx
+++ frontend/src/components/Welcome/Welcome.tsx
@@ -10,13 +10,13 @@ export function Welcome() {
           Mantine
         </Text>
       </Title>
-      <Text color="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
+      <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
         This starter Next.js project includes a minimal setup for server side rendering, if you want
         to learn more on Mantine + Next.js integration follow{' '}
         <Anchor href="https://mantine.dev/guides/next/" size="lg">
           this guide
         </Anchor>
-        . To get started edit index.tsx file.
+        . To get started edit page.tsx file.
       </Text>
     </>
   );
